62ef784f5cf06a850e33a41c85e93f9bba138a40
Serhio <seronly2013@gmail.com>
2025-09-23 14:19:37 +0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/chezmoi.lua

--- chezmoi/dot_config/nvim/lua/plugins/chezmoi.lua
+++ chezmoi/dot_config/nvim/lua/plugins/chezmoi.lua
@@ -8,5 +8,44 @@ return {
       -- add other options here if needed.
     end,
   },
+  {
+    'xvzc/chezmoi.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require("chezmoi").setup {
+        -- your configurations
+        edit = {
+          watch = false,
+          force = false,
+        },
+        events = {
+          on_open = {
+            notification = {
+              enable = true,
+              msg = "Opened a chezmoi-managed file",
+              opts = {},
+            },
+          },
+          on_watch = {
+            notification = {
+              enable = true,
+              msg = "This file will be automatically applied",
+              opts = {},
+            },
+          },
+          on_apply = {
+            notification = {
+              enable = true,
+              msg = "Successfully applied",
+              opts = {},
+            },
+          },
+        },
+        telescope = {
+          select = { "<CR>" },
+        },
+      }
+    end
+  },
 
 }
