8138b1cd999fc0b5e64d7e9ab4ac016d94ba7f16
seronly <seronly2013@gmail.com>
2025-11-29 14:20:01 +0700

    Update nvim

--- chezmoi/dot_config/nvim/lua/config/options.lua
+++ chezmoi/dot_config/nvim/lua/config/options.lua
@@ -3,21 +3,28 @@
 -- Add any additional options here
 
 -- Check if 'pwsh' is executable and set the shell accordingly
-if vim.fn.executable("pwsh") == 1 then
-  vim.o.shell = "pwsh"
+if vim.fn.has("win32") == 1 then
+  if vim.fn.executable("pwsh") == 1 then
+    vim.o.shell = "pwsh"
+  else
+    vim.o.shell = "powershell"
+  end
+  -- Setting shell command flags
+  vim.o.shellcmdflag =
+    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
+
+  -- Setting shell redirection
+  vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
+
+  -- Setting shell pipe
+  vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
 else
-  vim.o.shell = "powershell"
+  if vim.fn.executable("zsh") == 1 then
+    vim.o.shell = "/bin/zsh"
+  else
+    vim.o.shell = "/bin/bash"
+  end
 end
-
--- Setting shell command flags
-vim.o.shellcmdflag =
-  "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
-
--- Setting shell redirection
-vim.o.shellredir = '2>&1 | %{ "$_" } | Out-File %s; exit $LastExitCode'
-
--- Setting shell pipe
-vim.o.shellpipe = '2>&1 | %{ "$_" } | Tee-Object %s; exit $LastExitCode'
 
 -- Setting shell quote options
 vim.o.shellquote = ""
